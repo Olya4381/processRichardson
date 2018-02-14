63fc6f554d3a566af29d08510355f5ad00da87bd
Olga Nosova <nosova-olenka@amil.ru>
2018-02-15 01:37:34 +0300

    Исправлены ошибки при запуске.Переименованы переменные в классе посчета СКО. Подсчет Ско равномерно распр диаграмм.

--- py-visual/py_alpha.py
+++ py-visual/py_alpha.py
@@ -5,12 +5,13 @@ alph=0
 with open('C:\\Users\\nosov\\Documents\\Visual Studio 2015\\Projects\\Diag_up_git\\Diag_up\\alpha\\alpha_10x100_2.txt') as inf:
     alph_10x100=np.loadtxt(inf)
 
-with open('C:\\Users\\nosov\\Documents\\Visual Studio 2015\\Projects\\Diag_up_git\\Diag_up\\alpha_100x20_1.txt') as inf:
+with open('C:\\Users\\nosov\\Documents\\Visual Studio 2015\\Projects\\Diag_up_git\\Diag_up\\alpha_even_distr.txt') as inf:
     alph_100x20=np.loadtxt(inf)
 
 
 
 plt.figure()
-plt.plot(alph_10x100[:,0],alph_10x100[:,1],alph_100x20[:,0],alph_100x20[:,1])
-plt.title('Синий 10x100 \n Оранжевый 100x20')
+#plt.plot(alph_10x100[:,0],alph_10x100[:,1],alph_100x20[:,0],alph_100x20[:,1])
+plt.plot(alph_100x20[:,0],alph_100x20[:,1])
+#plt.title('Синий 10x100 \n Оранжевый 100x20')
 plt.show()
--- py-visual/py_visual.py
+++ py-visual/py_visual.py
@@ -1,10 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+#предельная форма планшереля
 x_p=np.linspace(-1,1,20)
 y_p=2/np.pi*(np.arcsin(x_p)*x_p+(1-x_p**2)**0.5)
 x=x_p+y_p
 y=-x_p+y_p
+#предельная форма равновероятного процесса
+x=np.linspace(0.0,5,100)
+y=np.log(1-np.exp(-np.pi/np.sqrt(6)*x_p))*-np.sqrt(6)/np.pi
+#опстроение
+#plt.figure()
+#plt.plot(x_p,y_p);
+#plt.show()
 
 
 with open('C:\\Users\\nosov\\Documents\\Visual Studio 2015\\Projects\\Diag_up_git\\Diag_up\\col_037_simple.txt') as inf:
@@ -17,7 +24,7 @@ with open('C:\\Users\\nosov\\Documents\\Visual Studio 2015\\Projects\\Diag_up_gi
 x3=(np.arange(col.shape[0])+1)/col.sum()**0.5
 y3=np.copy(col)/col.sum()**0.5
 
-with open('C:\\Users\\nosov\\Documents\\Visual Studio 2015\\Projects\\Diag_up_git\\Diag_up\\col_037_2m.txt') as inf:
+with open('C:\\Users\\nosov\\Documents\\Visual Studio 2015\\Projects\\Diag_up_git\\Diag_up\\file1\\col_037_2m.txt') as inf:
     col=np.loadtxt(inf)
 x1=(np.arange(col.shape[0])+1)/col.sum()**0.5
 y1=np.copy(col)/col.sum()**0.5
